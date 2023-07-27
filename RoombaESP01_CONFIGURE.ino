d57e475813afdbb209be050ff091076fc05f9141
minermartijn <minermartijn@gmail.com>
2023-07-27 13:21:05 +0200

    Added OTA, and trying to get songs to work.

--- RoombaESP01_CONFIGURE.ino
+++ RoombaESP01_CONFIGURE.ino
@@ -3,6 +3,8 @@
 #include <ESP8266mDNS.h>
 #include <SimpleTimer.h>
 #include <Roomba.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 
 //USER CONFIGURED SECTION START//
@@ -13,6 +15,11 @@ const int mqtt_port = YOUR_MQTT_SERVER_PORT;
 const char *mqtt_user = "YOUR_MQTT_USERNAME";
 const char *mqtt_pass = "YOUR_MQTT_PASSWORD";
 const char *mqtt_client_name = "Roomba"; // Client connections can't have the same connection name
+
+//pick an OTA password to use when updating over the air
+const char *OTApassword = "YOUR_OTA_PASSWORD";
+const int OTAport = 8266;
+
 //USER CONFIGURED SECTION END//
 
 
@@ -36,6 +43,7 @@ uint8_t tempBuf[10];
 
 void setup_wifi() 
 {
+  WiFi.hostname(mqtt_client_name);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) 
   {
@@ -65,6 +73,8 @@ void reconnect()
           boot = false;
         }
         // ... and resubscribe
+        songs();
+        ArduinoOTA.handle();
         client.subscribe("roomba/commands");
       } 
       else 
@@ -88,6 +98,10 @@ void callback(char* topic, byte* payload, unsigned int length)
   String newPayload = String((char *)payload);
   if (newTopic == "roomba/commands") 
   {
+    if (newPayload == "test")
+    {
+      client.publish("roomba/status", "Test New");
+    }
     if (newPayload == "start")
     {
       startCleaning();
@@ -96,9 +110,30 @@ void callback(char* topic, byte* payload, unsigned int length)
     {
       stopCleaning();
     }
+    if (newPayload == "restart")
+    {
+      client.publish("roomba/status", "Rebooting");
+      ESP.restart();
+    }
+    if (newPayload == "installsongfour")
+    {
+      songs();
+
+    }
+    if (newPayload == "songone")
+    {
+      playSongOne();
+    }
+    if (newPayload == "songtwo")
+    {
+      playSongTwo();
+    }
+    if (newPayload == "songthree")
+    {
+      playSongThree();
+    }
   }
 }
-
 
 void startCleaning()
 {
@@ -118,6 +153,42 @@ void stopCleaning()
   delay(50);
   Serial.write(143);
   client.publish("roomba/status", "Returning");
+}
+
+void playSongOne()
+{
+  Serial.write(128);
+  delay(50);
+  Serial.write(131);
+  delay(50);
+  byte Playsong1[2]={141,0}; //Strange Encounters
+  Serial.write(Playsong1,2);
+  client.publish("roomba/status", "Singing song One");
+}
+
+void playSongTwo()
+{
+  Serial.write(128);
+  delay(50);
+  Serial.write(131);
+  delay(50);
+  byte Playsong2[2]={141,1}; //Strange Encounters
+  Serial.write(Playsong2,2);
+  client.publish("roomba/status", "Singing song Two");
+}
+
+void playSongThree()
+{
+  Serial.write(128);
+  delay(50);
+  Serial.write(131);
+  delay(50);
+  byte Playsong3[2]={141,2}; //Strange Encounters
+  Serial.write(Playsong3,2);
+  delay(5000);
+  byte Playsong4[2]={141,3}; //Strange Encounters
+  Serial.write(Playsong4,2);
+  client.publish("roomba/status", "Singing song Three");
 }
 
 void sendInfoRoomba()
@@ -147,8 +218,6 @@ void sendInfoRoomba()
   client.publish("roomba/charging", battery_Current_mAh_send);
 }
 
-
-
 void setup() 
 {
   Serial.begin(115200);
@@ -156,15 +225,37 @@ void setup()
   delay(50);
   Serial.write(11);
   delay(50);
-  WiFi.mode(WIFI_STA);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
   timer.setInterval(5000, sendInfoRoomba);
+
+  ArduinoOTA.setPort(OTAport);
+  ArduinoOTA.setHostname(mqtt_client_name);
+  ArduinoOTA.setPassword((const char *)OTApassword);
+  ArduinoOTA.begin();
 }
+
+void songs() {
+  byte Song0[17]={140,0,7,31,60,43,55,55,50,67,45,79,40,91,35,103,30};
+  byte Song1[35]={140,1,16,36,20,37,20,0,40,36,20,37,20,0,30,36,20,37,20,0,20,36,20,37,20,0,10,36,20,37,20,0,5,38,64}; //Jaws
+  byte Song2[13]={140,2,5,79,48,81,48,77,48,65,50,72,64}; //Strange Encounters 1
+  byte Song3[13]={140,3,5,69,48,71,48,67,48,55,48,62,64}; //Strange Encounters
+  Serial.write(128);
+  delay(50);
+  Serial.write(131);
+  delay(50);
+  Serial.write(Song0,17);
+  Serial.write(Song1,35);
+  Serial.write(Song2,13);
+  Serial.write(Song3,13);
+  client.publish("checkIn/roomba", "Songs Installed");
+}
+
 
 void loop() 
 {
+  ArduinoOTA.handle();
   if (!client.connected()) 
   {
     reconnect();
@@ -172,5 +263,3 @@ void loop()
   client.loop();
   timer.run();
 }
-
-
